3648a77bcca124962d9eaa584e9e67d9a3ff0f23
Ari Vaniderstine <hack@ariari.io>
2016-02-21 11:05:31 -0500

    Grab codes from a text file and find word for each

--- decode.py
+++ decode.py
@@ -1,6 +1,14 @@
-code = "5:1."
-
+# Open the cipher text and find the appropriate line
 ciphertext = open("book.txt", "r")
-for line in ciphertext:
-    if code in line: print line
+code = open("code.txt", "r")
+
+for line in code:
+  linecode = line.split()[0] + ":" + line.split()[1]
+  wordcode = line.split()[2]
+
+  for line in ciphertext:
+      if linecode in line:
+        print line.split()[int(wordcode)]
+        
+code.close()
 ciphertext.close()
